a507e5a7002befe28f5eb5dcf1354c44bbc60a1c
Nathan Snow <nathan.snow@cqumail.com>
2022-08-27 19:32:30 +1000

    Initial security config for dev profile

--- config/src/main/java/com/wilma/config/security/SecurityConfigDEV.java
+++ config/src/main/java/com/wilma/config/security/SecurityConfigDEV.java
@@ -10,24 +10,40 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-
+/**
+ * Spring security configuration used while in the DEV profile.
+ */
 @Configuration
 @ComponentScan
 @Profile("dev")
 public class SecurityConfigDEV {
+
+    /**
+     * Instantiates an instance of a custom user details service which can then be auto-wired throughout the application
+     * @return A custom user details service implementation
+     */
     @Bean
     public UserDetailsService userDetailsService(){
         return new CustomUserDetailsService();
     }
 
+    /**
+     * Instantiates an instance of the chosen password encoder which can then be auto-wired throughout the application
+     * @return The chosen password encoder implementation
+     */
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    /**
+     * Defines security filters to be implemented by the application
+     * @param http The http security builder
+     * @return A security filter chain containing the defined filers
+     * @throws Exception Multiple exception possibilities
+     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        //Enable all requests (no security)
         http.authorizeRequests().antMatchers("/").permitAll();
 
         //Disable cross site referencing and header frames for H2 database
