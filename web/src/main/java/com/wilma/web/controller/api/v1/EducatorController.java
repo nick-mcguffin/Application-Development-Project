2a8cedae3d7e33286a98a9fd68a39e7787bd554b
Nathan Snow <nathan.snow@cqumail.com>
2022-08-27 19:38:18 +1000

    Initial controllers and exception handling

--- web/src/main/java/com/wilma/web/controller/api/v1/EducatorController.java
+++ web/src/main/java/com/wilma/web/controller/api/v1/EducatorController.java
@@ -1,16 +1,19 @@
-package com.wilma.web.controller.api;
+package com.wilma.web.controller.api.v1;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Educator REST API
+ */
 @RestController
-@RequestMapping("/educators")
+@RequestMapping("${api.v1.context-path}/educators")
 public class EducatorController {
 
     @GetMapping
     public ResponseEntity<?> test(){
-        return ResponseEntity.ok("Hello from the educator controller");
+        return ResponseEntity.ok("Hello from the educator API");
     }
 }
--- web/src/main/java/com/wilma/web/controller/api/v1/StudentController.java
+++ web/src/main/java/com/wilma/web/controller/api/v1/StudentController.java
@@ -1,14 +1,17 @@
-package com.wilma.web.controller.api;
+package com.wilma.web.controller.api.v1;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Student REST API
+ */
 @RestController
-@RequestMapping("/students")
+@RequestMapping("${api.v1.context-path}/students")
 public class StudentController {
 
     @GetMapping
     public ResponseEntity<?> test(){
-        return ResponseEntity.ok("Hello from the student controller");
+        return ResponseEntity.ok("Hello from the student API");
     }
 }
