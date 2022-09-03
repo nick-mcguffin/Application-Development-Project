b9773a2c0e2b48df9cc62a10afc5b041977af6af
Nathan Snow <nathan.snow@cqumail.com>
2022-09-03 20:36:09 +1000

    Added dummy data

--- web/src/main/java/com/wilma/web/controller/web/portal/EducatorPortalController.java
+++ web/src/main/java/com/wilma/web/controller/web/portal/EducatorPortalController.java
@@ -12,7 +12,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 import java.time.Period;
 import java.util.Date;
 import java.util.List;
@@ -32,21 +31,25 @@ public class EducatorPortalController {
     }
 
     @GetMapping("/marketplace")
-    public String marketplace(Model model) {
+    public String marketplace(Model model) {   
         model.addAllAttributes(Map.of(
                 "currentPage", "marketplace",
                 "menuElements", UserConfiguration.educatorMenuElements,
-                "positions", List.of(
-                    new Job(1, new Partner(), new Date(), new Date(), Period.of(0,0,1), "Brisbane", "A sample job", false, 25.50, PayType.WAGE, Frequency.WEEKLY),
-                    new Job(2, new Partner(), new Date(), new Date(), Period.of(0,11,1), "Perth", "A 2nd sample job", false, 27.50, PayType.WAGE, Frequency.WEEKLY),
-                    new Placement(3, new Partner(), new Date(), new Date(), Period.of(0,3,0), "Sydney", "A placement example", false, false)
+                "approvedPositions", List.of(
+                    new Job(1, new Partner("Microsoft"), new Date(), new Date(), Period.of(0,0,1), "Brisbane", "A sample job", false, 25.50, PayType.WAGE, Frequency.WEEKLY),
+                    new Job(2, new Partner("Google"), new Date(), new Date(), Period.of(0,11,1), "Perth", "A 2nd sample job", false, 27.50, PayType.WAGE, Frequency.WEEKLY),
+                    new Placement(3, new Partner("Apple"), new Date(), new Date(), Period.of(1,0,0), "Sydney", "A placement example", false, false)
+                ),
+                "pendingPositions", List.of(
+                    new Job(1, new Partner("Microsoft"), new Date(), new Date(), Period.of(0,0,1), "Brisbane", "A sample job", false, 25.50, PayType.WAGE, Frequency.WEEKLY),
+                    new Job(2, new Partner("Google"), new Date(), new Date(), Period.of(0,11,1), "Perth", "A 2nd sample job", false, 27.50, PayType.WAGE, Frequency.WEEKLY),
+                    new Placement(3, new Partner("Apple"), new Date(), new Date(), Period.of(1,0,0), "Sydney", "A placement example", false, false)
                 )
         ));
         return "/educator/marketplace";
     }
     /*
     Todo:
-        - ADP-71: Jobs & Placements
         - ADP-74: Jobs & Placement Management
         - ADP-72: Forum
         - ADP-75: Forum Management
