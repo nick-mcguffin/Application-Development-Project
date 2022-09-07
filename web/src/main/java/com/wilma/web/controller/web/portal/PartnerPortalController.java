a597a051417ba1942760d9b75e4fcc5ea2802e8a
AcmeCat <allenruaidhri@gmail.com>
2022-09-07 14:56:29 +0800

    Added dashboard.html and added mapping in PartnerPortalController

--- web/src/main/java/com/wilma/web/controller/web/portal/PartnerPortalController.java
+++ web/src/main/java/com/wilma/web/controller/web/portal/PartnerPortalController.java
@@ -1,12 +1,27 @@
 package com.wilma.web.controller.web.portal;
 
+import com.wilma.config.web.UserConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
 
 @Controller
+@RequestMapping ("/partner")
 public class PartnerPortalController {
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model) {
+        model.addAllAttributes(Map.of(
+                "currentPage", "dashboard",
+                "menuElements", UserConfiguration.partnerMenuElements
+        ));
+        return "/partner/dashboard";
+    }
     /*
     Todo:
-        - ADP-76: Dashboard
         - ADP-77: Jobs & Placements
         - ADP-78: Forum
         - ADP-79: Job & Placement Management
