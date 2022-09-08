f113d726be3a3f46d5f80ae7eec1311ea7324695
Ryulightorb <Badstar4@hotmail.com.au>
2022-09-08 14:27:00 +1000

    Added Student Profile with changes to fit the local variables.

--- web/src/main/java/com/wilma/web/controller/web/portal/StudentPortalController.java
+++ web/src/main/java/com/wilma/web/controller/web/portal/StudentPortalController.java
@@ -1,8 +1,15 @@
 package com.wilma.web.controller.web.portal;
 
+import com.wilma.config.web.UserConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
 
 @Controller
+@RequestMapping("/student")
 public class StudentPortalController {
     /*
     Todo:
@@ -12,4 +19,13 @@ public class StudentPortalController {
         - ADP-68: Resume Management
         - ADP-69: Profile
      */
+
+    @GetMapping("/studentProfile")
+    public String studentProfile(Model model) {
+        model.addAllAttributes(Map.of(
+                "currentPage", "profile",
+                "menuElements", UserConfiguration.studentMenuElements
+        ));
+        return "/student/studentProfile";
+    }
 }
