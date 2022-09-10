783795fd5f5f60af748799d3b9551f8f8e3475e8
Nick McGuffin <nicholas.mcguffin@cqumail.com>
2022-09-10 23:13:47 +1000

    Add UI for student dashboard.

--- web/src/main/java/com/wilma/web/controller/web/portal/StudentPortalController.java
+++ web/src/main/java/com/wilma/web/controller/web/portal/StudentPortalController.java
@@ -1,9 +1,27 @@
 package com.wilma.web.controller.web.portal;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.wilma.config.web.UserConfiguration;
 
 @Controller
+@RequestMapping ("/student")
 public class StudentPortalController {
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model) {
+        model.addAllAttributes(Map.of(
+                "currentPage", "dashboard",
+                "menuElements", UserConfiguration.studentMenuElements
+        ));
+        return "/student/dashboard";
+    }
+    
     /*
     Todo:
         - ADP-65: Dashboard
