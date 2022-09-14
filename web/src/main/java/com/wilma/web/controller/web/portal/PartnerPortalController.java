d7168e4286899689be6cec027057bc844d3a4c27
Nathan Snow <nathan.snow@cqumail.com>
2022-09-14 10:30:25 +1000

    Fixed missing curly brace and imports

--- web/src/main/java/com/wilma/web/controller/web/portal/PartnerPortalController.java
+++ web/src/main/java/com/wilma/web/controller/web/portal/PartnerPortalController.java
@@ -15,9 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 
@@ -80,6 +78,7 @@ public class PartnerPortalController {
                 "positionType", type
         ));
         return "/partner/edit-position";
+    }
 
     @GetMapping("/forum")
     public String forumOverview(Model model) {
