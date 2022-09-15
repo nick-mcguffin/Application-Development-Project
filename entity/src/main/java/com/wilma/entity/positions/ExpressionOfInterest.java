64bdeee034b1757323928dbea32b44290f95f5d2
Nick McGuffin <nicholas.mcguffin@cqumail.com>
2022-09-15 12:36:21 +1000

    Remake expression of interest entity

--- entity/src/main/java/com/wilma/entity/positions/ExpressionOfInterest.java
+++ entity/src/main/java/com/wilma/entity/positions/ExpressionOfInterest.java
@@ -1,12 +1,18 @@
 package com.wilma.entity.positions;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import java.time.Period;
 import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 import com.wilma.entity.users.Partner;
 
@@ -20,27 +26,44 @@ import lombok.Setter;
 @Entity
 @NoArgsConstructor
 @Table(name = "expression_of_interest")
-public class ExpressionOfInterest extends PositionCategory {
+public class ExpressionOfInterest{
     
-    @Column(name = "author_id")
-    private Integer authorId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Integer id;
 
-    @Column(name = "category_id")
-    private Integer categoryId;
+    @ManyToOne
+    @JoinColumn(name = "partner_id")
+    private Partner partner;
 
-    private String title;
-    
-    private String positionType;
+    @Column(name = "start_date")
+    private Date startDate;
 
-    private Boolean current;
+    @Column(name = "end_date")
+    private Date endDate;
 
-    private Integer partnerId;
+    private Period period;
+    private String location;
+    private String description;
+    private boolean filled;
 
-    public ExpressionOfInterest(Integer id, Partner partner, Date startDate, Date endDate, Period period, String location, String description, boolean filled, boolean approved, String categoryName, String title, String positionType, Boolean current) {
-        super(id, partner, startDate, endDate, period, location, description, filled, approved, categoryName);
-        this.title = title;
-        this.current = current;
-        this.positionType = positionType;
+    private boolean approved;
+
+    @Transient
+    private String type;
+    //Category
+
+    public ExpressionOfInterest(Integer id, Partner partner, Date startDate, Date endDate, Period period, String location, String description, boolean filled, boolean approved) {
+        this.id = id;
+        this.partner = partner;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.period = period;
+        this.location = location;
+        this.description = description;
+        this.filled = filled;
+        this.approved = approved;
+        this.type = this.getClass().getSimpleName();
     }
-
 }
