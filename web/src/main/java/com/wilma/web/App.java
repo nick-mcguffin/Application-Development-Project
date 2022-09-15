4e01d4e72a67ef1afefcf99051e798447252e0ba
Nathan Snow <nathan.snow@cqumail.com>
2022-09-15 11:30:18 +1000

    Create mailer bean so it can be autowired

--- web/src/main/java/com/wilma/web/App.java
+++ web/src/main/java/com/wilma/web/App.java
@@ -1,8 +1,11 @@
 package com.wilma.web;
 
+import com.wilma.service.mail.Mailer;
+import com.wilma.service.mail.MailerImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -17,4 +20,8 @@ public class App {
         SpringApplication.run(App.class);
     }
     
+    @Bean
+    public Mailer mailer(){
+        return new MailerImpl();
+    }
 }
