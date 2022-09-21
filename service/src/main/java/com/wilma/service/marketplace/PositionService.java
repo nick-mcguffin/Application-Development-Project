173ba4f553a2e886e9164d6f1d87696601721e38
AcmeCat <allenruaidhri@gmail.com>
2022-09-22 00:39:05 +0800

    new service currently extends CrudOpsImpl. adds methods to create and fetch job data

---
+++ service/src/main/java/com/wilma/service/marketplace/PositionService.java
@@ -0,0 +1,41 @@
+package com.wilma.service.marketplace;
+
+        import com.wilma.entity.dto.JobDTO;
+        import com.wilma.entity.positions.Job;
+        import com.wilma.entity.positions.Position;
+        import com.wilma.repository.JobRepository;
+        import com.wilma.repository.PositionRepository;
+        import com.wilma.service.UserService;
+        import com.wilma.service.forum.CrudOpsImpl;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.beans.factory.annotation.Value;
+        import org.springframework.security.core.context.SecurityContextHolder;
+        import org.springframework.stereotype.Service;
+        import java.util.List;
+
+@Service
+public class PositionService extends CrudOpsImpl<Position, Integer, PositionRepository> {
+
+    @Autowired
+    PositionRepository positionRepository;
+
+    @Autowired
+    JobRepository jobRepository;
+    @Autowired
+    UserService userService;
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
+
+    public Job addJobFromDTO(JobDTO jobDTO) {
+
+        var currentUser = activeProfile.equalsIgnoreCase("prod")?
+                userService.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName()) : userService.findByUsername("educator");
+        var job = new Job(null, jobDTO.getPartner(), jobDTO.getStartDate(), jobDTO.getEndDate(), jobDTO.getPeriod(), jobDTO.getLocation(), jobDTO.getDescription(), false, false, jobDTO.getPayRate(), jobDTO.getPayType(), jobDTO.getPayFrequency());
+
+        return jobRepository.save(job);
+    }
+
+    public List<Job> getJobs(){
+        return jobRepository.findAll();
+    }
+}
