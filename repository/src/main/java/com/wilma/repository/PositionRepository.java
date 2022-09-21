3b6278aa029a5a8cc031f737807664ec45e8bec4
AcmeCat <allenruaidhri@gmail.com>
2022-09-22 00:33:20 +0800

    repository interfaces for position, job, and placement. Not sure if I need all of them

---
+++ repository/src/main/java/com/wilma/repository/PositionRepository.java
@@ -0,0 +1,9 @@
+package com.wilma.repository;
+
+import com.wilma.entity.positions.Position;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PositionRepository extends JpaRepository<Position, Integer> {
+}
