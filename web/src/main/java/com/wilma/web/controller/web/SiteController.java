ed7811f2e3c221b16dd4457ee54b8c4dfc001a4f
Nick McGuffin <nicholas.mcguffin@cqumail.com>
2022-09-25 10:07:51 +1000

    Implement email on submitting contact form.

--- web/src/main/java/com/wilma/web/controller/web/SiteController.java
+++ web/src/main/java/com/wilma/web/controller/web/SiteController.java
@@ -1,10 +1,16 @@
 package com.wilma.web.controller.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.wilma.entity.contact.ContactForm;
+import com.wilma.service.mail.Mailer;
 
 import java.util.List;
 import java.util.Map;
@@ -14,6 +20,9 @@ import java.util.Map;
  */
 @Controller
 public class SiteController {
+
+    @Autowired
+    protected Mailer mailer;
 
     @GetMapping("/")
     public String home(Model model) {
@@ -45,6 +54,22 @@ public class SiteController {
         return "/contact";
     }
 
+    @PostMapping("/submit")
+    public String contactSubmit(@ModelAttribute ContactForm contactForm, Model model) {
+
+        model.addAllAttributes(Map.of(
+            "currentPage", "Contact Us"));
+
+        String mailerMessage = "From: " + contactForm.getName() + "\n\n" + "Contact Email: " + contactForm.getEmail() + "\n\n" + "Message: " + contactForm.getDetails();
+        
+        mailer.sendEmail("nicholas.mcguffin@gmail.com", "WILMA: Contact Us Request Received",  mailerMessage);
+
+        return "/contact";
+    }
+        //autowiring mailing interface
+        //call send email function to => 
+    // }
+    
     @RequestMapping("/login")
     public String login(Model model) {
         model.addAttribute("currentPage", "Login");
