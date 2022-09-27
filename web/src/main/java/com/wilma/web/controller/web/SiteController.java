820549b454d58253b0b8050bc12344cc41170c3d
Nathan Snow <nathan.snow@cqumail.com>
2022-09-27 19:06:30 +1000

    Added view for students to see (readonly) placement details

--- web/src/main/java/com/wilma/web/controller/web/SiteController.java
+++ web/src/main/java/com/wilma/web/controller/web/SiteController.java
@@ -1,7 +1,6 @@
 package com.wilma.web.controller.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
