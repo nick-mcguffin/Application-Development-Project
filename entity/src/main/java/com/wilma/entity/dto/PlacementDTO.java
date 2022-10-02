a97c116f45209ae9a9fc19ee9edf419395dc7828
Nathan Snow <nathan.snow@cqumail.com>
2022-10-03 07:24:34 +1000

    Corrected the period value generation

--- entity/src/main/java/com/wilma/entity/dto/PlacementDTO.java
+++ entity/src/main/java/com/wilma/entity/dto/PlacementDTO.java
@@ -20,31 +20,25 @@ public class PlacementDTO {
     private String location;
     private String description;
     private boolean filled;
-
     private boolean approved;
-
     private boolean completed;
-
     private String review;
 
-    public PlacementDTO(Integer id, Partner partner, Date startDate, Date endDate, String location, String description, boolean filled, boolean approved, boolean completed,String review) {
-        this.id = id;
-        this.partner = partner;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.period = Period.between(
-                startDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate(),
-                endDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate()
-        );
-        this.location = location;
-        this.description = description;
-        this.filled = filled;
-        this.approved = approved;
-        this.completed = completed;
-        this.review = review;
-    }
 
-    public PlacementDTO() {
-
+    /**
+     * Returns the {@link Period} between the placement start and end dates (if provided, zero days otherwise) by first
+     * <a href="https://stackoverflow.com/questions/21242110/convert-java-util-date-to-java-time-localdate/21242111#21242111">converting the Date objects into LocalDate objects</a>
+     * @return the duration between the start and end dates <b>plus one day</b>
+     */
+    public Period getPeriod() {
+        if (startDate != null && endDate != null) {
+            this.period = Period.between(
+                    startDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate(),
+                    endDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate()
+            )
+            .plusDays(1);
+        }
+        else this.period = Period.ZERO;
+        return this.period;
     }
 }
